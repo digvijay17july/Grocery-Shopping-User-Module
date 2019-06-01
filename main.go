bed063eb5dc24add8b60c7bf390567ba7071e526
Digvijay Singh <Digvijay.Singh@BDINDIA.LOCAL>
2019-06-01 20:46:08 +0530

    logs generated

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"Grocery-Shopping-User-Module/src/app/api"
+	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"os"
 )
 func main() {
+	fmt.Print("Starting User Module.... ")
 	config := api.GetConfig()
 
+	fmt.Print("Configuration Values :" +config.DB.Host)
 	app := &api.App{}
 	app.Initialize(config)
+	fmt.Print("Port No. is :"+os.Getenv("PORT"))
 	app.Run(os.Getenv("PORT"))
+
+	fmt.Print("Started User Module.... ")
 }
